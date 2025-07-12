5bf8781f135bf2386bb1739078b192ea23f9841d
V1ola1ne <henning@cdwalter.de>
2025-07-12 20:15:40 +0200

    improved Readability

--- Winget Automatic Update.psm1
+++ Winget Automatic Update.psm1
@@ -1,7 +1,27 @@
 Update-Packages {
+
+    Write-Verbose "Getting all available Updates"
+
     $UpgradeArray = @((winget list --upgrade-available) -match "[A-Za-z0-9]\.[A-Za-z0-9]")
-    $list = ($UpgradeArray -split ' ') -match "[A-Za-z0-9]\.[A-Za-z0-9]" -notmatch "[0-9]\.[0-9]"
-    foreach ($package in $list) {
-        winget upgrade $package
+    if ($null -eq $UpgradeArray) {
+
+        [System.Console]::WriteLine("No Packages need to be updated")
+        exit
+
+    } else {
+
+        $list = ($UpgradeArray -split ' ') -match "[A-Za-z0-9]\.[A-Za-z0-9]" -notmatch "[0-9]\.[0-9]"
+
+        Write-Verbose "These Packages need to be upgraded`n$list"
+
+        foreach ($package in $list) {
+
+            winget upgrade $package
+
+        }
+
+        [System.Console]::WriteLine("All packages have been updated")
+
     }
+
 }
